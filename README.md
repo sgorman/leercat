76bd6807b955dc1c8caf449a4dd117e4478ccdec
Scott Gorman <sgorman07@gmail.com>
2014-08-07 15:40:41 -0400

    the leerest of them all

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 leercat
 =======
 
-leercat is currently just a twitter script written in ruby. it uses the twitter gem.
+leercat is currently just a twitter app written in ruby. it uses the twitter gem and also as of 8/5/14 it uses the whatlanguage gem.
 
-what does it do?
-================
+leercat is designed to be ran on your own account as an application. it can perform various functions, however due to the twitter ToS, misusing leercat will get your account locked. as of right now, there are various procedures that i have written but have not defined a clear path of control for the client. there is a lot of work to be done.
 
-running it will look through all your followers then unfollow anyone who does not follow you back.
+leercat can currently
+
+- find and (un)follow gain tweet spammers
+- unfollow all unfollowers
+- unfollow people not tweeting in your native language
 
 setup
 =====
 
 `gem install twitter`
+`gem install whatlanguage`
 
 - edit the config.rb with your twitter app settings
 - make sure you give your app full permissions
--- leercat.rb
+++ leercat.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'Twitter'
+require 'whatlanguage'
 load 'config.rb'
 
 MAX_ATTEMPTS = 1000
@@ -9,6 +10,7 @@ class Leercat
 	def initialize(config)
 		@client = Twitter::REST::Client.new(config)
 		@stream = Twitter::Streaming::Client.new(config)
+		@whatlanguage = WhatLanguage.new(:all)
 	end
 
 	def create_following_list()
@@ -109,12 +111,99 @@ class Leercat
 		end
 	end
 
+	def find_annoying_spammers()
+		spammer_list = []
+		@stream.user do |object|
+			case object
+			when Twitter::Tweet
+
+				if object.text.include?("#ＲＥＴＷＥＥＴ") || object.text.include?("#TFB_Cats") || object.text.include?("RT2GAIN") || object.text.include?("FOLLOWBACK") || object.text.include?("#FOLLOW") || object.text.include?("followback") || object.text.include?("#MGWV") || object.text.include?("#AnotherFollowTrain") || object.text.include?("FOLLOWTRICK") || object.text.include?("#TEAMFOLLOWBACK") || object.text.include?("#TEAMMZBNIKKI")
+					if !spammer_list.include?(object.user.id)
+						spammer_list.push(object.user.id)
+						puts "Spammer count: #{spammer_list.length}"
+					end
+				end
+			end
+
+			if 	spammer_list.length >= 10
+				return spammer_list
+			end
+		end
+	end
+
+	def is_spam_tweet(text)
+		if text.include?("#ＲＥＴＷＥＥＴ") || text.include?("#TFB_Cats") || text.include?("RT2GAIN") || text.include?("FOLLOWBACK") || text.include?("#FOLLOW") || text.include?("followback") || text.include?("#MGWV") || text.include?("#AnotherFollowTrain") || text.include?("FOLLOWTRICK") || text.include?("#TEAMFOLLOWBACK") || text.include?("#TEAMMZBNIKKI")
+			return true
+		end
+
+		return false
+	end
+
+	def is_foreign_tweet(text)
+		if @whatlanguage.language(text) != "english"
+			return true
+		end
+
+		return false
+	end
+
+	def find_foreign_tweeters()
+		foreign_list = []
+		@stream.user do |object|
+			case object
+			when Twitter::Tweet
+
+				if @whatlanguage.language(object.text) != "english"
+					if !foreign_list.include?(object.user.id)
+						foreign_list.push(object.user.id)
+						puts "Foreign count: #{foreign_list.length}"
+					end
+				end
+			end
+
+			if 	foreign_list.length >= 10
+				return foreign_list
+			end
+		end
+	end	
+
+	def find_people_to_unfollow(count=100, max_read=1000)
+		unfollow_list = []
+		read = 0
+		@stream.user do |object|
+			case object
+			when Twitter::Tweet
+				read += 1
+				add_to_list = false
+
+				if is_foreign_tweet(object.text) || is_spam_tweet(object.text)
+					if !unfollow_list.include?(object.user.id)
+						unfollow_list.push(object.user.id)
+						puts "Unfollow list count: #{unfollow_list.length}"
+					end
+				end
+			end
+
+			if unfollow_list.length >= count || read >= max_read
+				return unfollow_list
+			end
+
+
+		end		
+	end
 end
 
 leer = Leercat.new($config)
+#leer.create_following_list()
+#unfollow_ids = leer.create_unfollower_list();
+#leer.bulk_unfollow(unfollow_ids)
+#leer.bulk_unfollow(leer.find_annoying_spammers())
+while true do
+	leer.bulk_unfollow(leer.find_people_to_unfollow())
+end
+=begin
 leer.create_following_list()
 followed_count = 0
-
 while true do
 	if followed_count >= 200
 		leer.create_following_list()
@@ -126,3 +215,5 @@ while true do
 	leer.bulk_follow(follow_these_ids)
 	followed_count += 15
 end
+=end
+
